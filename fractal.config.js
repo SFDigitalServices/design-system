2c4c94134eafb7ac361bb31c8aa456a90778982b
Shawn Allen <shawnbot@users.noreply.github.com>
2021-01-13 23:18:25 -0800

    twiddle knobs in fractal.config.js

--- fractal.config.js
+++ fractal.config.js
@@ -16,60 +16,66 @@ const {
 const gitSHA = VERCEL_GIT_COMMIT_SHA || getGitSHA()
 const gitRef = VERCEL_GIT_COMMIT_REF || getGitRef()
 
-const information = [
-  {
-    label: 'Built on',
-    value: new Date(),
-    type: 'time',
-    format (value) {
-      return value.toLocaleDateString('en')
-    }
-  },
-  {
-    label: 'Version',
-    value: pkg.version,
-    format (value) {
-      return `<a href="https://unpkg.com/${pkg.name}@${value}/">${value}</a>`
-    }
-  },
-  gitRef && {
-    label: 'Branch',
-    value: gitRef,
-    format (value) {
-      return `<a href="https://github.com/${pkg.repository}/compare/${value}">${value}</a>`
-    }
-  },
-  gitSHA && {
-    label: 'Commit',
-    value: gitSHA,
-    format (value) {
-      const short = value.substr(0, 7)
-      return `<a href="https://github.com/${pkg.repository}/commit/${value}">${short}</a>`
-    }
-  },
-  VERCEL_ENV && {
-    label: 'Environment',
-    value: VERCEL_ENV
-  },
-].filter(Boolean)
-
 const theme = mandelbrot({
   skin: 'white',
   nav: ['search', 'docs', 'components', 'information'],
   panels: ['html', 'view', 'context', 'resources', 'info', 'notes'],
-  information
+  styles: [
+    'default',
+    '/css/fractal.css'
+  ],
+  scripts: [
+    'default',
+    '/js/fractal.js'
+  ],
+  information: [
+    {
+      label: 'Built on',
+      value: new Date(),
+      type: 'time',
+      format (value) {
+        return value.toLocaleDateString('en')
+      }
+    },
+    {
+      label: 'Version',
+      value: pkg.version,
+      format (value) {
+        return `<a href="https://unpkg.com/${pkg.name}@${value}/">${value}</a>`
+      }
+    },
+    gitRef && {
+      label: 'Branch',
+      value: gitRef,
+      format (value) {
+        return `<a href="https://github.com/${pkg.repository}/compare/${value}">${value}</a>`
+      }
+    },
+    gitSHA && {
+      label: 'Commit',
+      value: gitSHA,
+      format (value) {
+        const short = value.substr(0, 7)
+        return `<a href="https://github.com/${pkg.repository}/commit/${value}">${short}</a>`
+      }
+    },
+    VERCEL_ENV && {
+      label: 'Environment',
+      value: VERCEL_ENV
+    },
+  ].filter(Boolean)
 })
 
 theme.addLoadPath('src/templates')
 
 fractal.web.theme(theme)
 fractal.web.set('builder.dest', 'dist')
+fractal.web.set('static.path', 'dist')
 
 fractal.set('project.title', pkg.project.title)
 fractal.set('project.package', pkg)
 
 const defaultContext = {
-  package: pkg
 }
 
 const nunjucks = require('@frctl/nunjucks')({
