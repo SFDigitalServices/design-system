c04204ed06a837630844f1839091e6732a43d39e
Shawn Allen <shawnbot@users.noreply.github.com>
2021-05-17 10:11:45 -0700

    rename <style-descriptor> -> <computed-style>, add unit support

--- src/js/preview.js
+++ src/js/preview.js
@@ -1,14 +1,33 @@
-window.customElements.define('style-descriptor', class StyleDescriptor extends HTMLElement {
+window.customElements.define('computed-style', class extends HTMLElement {
   connectedCallback () {
+    this.style.display = 'block'
+    this.render()
+    window.addEventListener('resize', () => this.render())
+  }
+
+  render () {
     const target = this.getAttribute('data-target')
     const el = target ? document.querySelector(target) : this
     const style = window.getComputedStyle(el)
     const targets = this.querySelectorAll('[data-css-property]')
     for (const target of targets) {
       const prop = target.getAttribute('data-css-property')
-      const value = style.getPropertyValue(prop)
+      const rawValue = style.getPropertyValue(prop)
+      const { value, units } = this.parseValue(rawValue)
       target.setAttribute('data-css-value', value)
-      target.textContent = value
+      target.setAttribute('data-css-units', units)
+      target.textContent = target.getAttribute('data-units') === 'false'
+        ? value
+        : rawValue
+    }
+  }
+
+  parseValue (value) {
+    const match = value.match(/^(?<value>.+)(?<units>px|%|r?em)$/)
+    if (match) {
+      return match.groups
+    } else {
+      return { value, units: undefined }
     }
   }
 })
