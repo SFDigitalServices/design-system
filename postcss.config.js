af951b0c45042b81e8c099dc2fa81e9d73aa9ddf
Shawn Allen <shawnbot@users.noreply.github.com>
2021-05-24 09:54:31 -0700

    use postcss-copy to copy referenced font files

--- postcss.config.js
+++ postcss.config.js
@@ -11,7 +11,13 @@ module.exports = {
     require('autoprefixer'),
     require('postcss-custom-properties'),
     prod && require('cssnano'),
-    require('postcss-normalize-charset')
+    require('postcss-normalize-charset'),
+    require('postcss-copy')({
+      basePath: ['src', 'node_modules'],
+      preservePath: true,
+      dest: 'dist/css/files',
+      template: '[name].[ext]'
+    })
   ].filter(Boolean),
   map: {
     inline: false
