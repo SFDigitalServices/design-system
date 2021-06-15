09f20e568860cc0bb54f9e8ff7e2b3a56f720787
Shawn Allen <shawnbot@users.noreply.github.com>
2021-06-15 14:48:39 -0700

    add eleventy nav plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,13 @@
+const navigation = require('@11ty/eleventy-navigation')
 const yaml = require('js-yaml')
 
 module.exports = config => {
+  config.addPlugin(navigation)
+
   config.addDataExtension('yml', contents => yaml.safeLoad(contents))
+
   config.addPassthroughCopy('dist')
+  config.addWatchTarget('dist')
 
   return {
     dir: {
