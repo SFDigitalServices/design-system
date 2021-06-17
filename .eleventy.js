1204bbdfd66aec6f2955d5c69622b48629ca7fed
Shawn Allen <shawnbot@users.noreply.github.com>
2021-06-16 20:57:32 -0700

    set up remark plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,11 @@
+const remark = require('@fec/eleventy-plugin-remark')
+const remarkConfig = require('./lib/remark').eleventyPluginConfig
 const navigation = require('@11ty/eleventy-navigation')
 const yaml = require('js-yaml')
 
 module.exports = config => {
   config.addPlugin(navigation)
+  config.addPlugin(remark, remarkConfig)
 
   config.addDataExtension('yml', contents => yaml.safeLoad(contents))
 
