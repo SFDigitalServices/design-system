8d1b62d04aebdf9abad5a54b08758a168a9e76bf
Shawn Allen <shawnbot@users.noreply.github.com>
2021-06-17 13:12:26 -0700

    simplify font css loading

--- docs/_data/assets.js
+++ docs/_data/assets.js
@@ -1,33 +1,8 @@
-const { URLSearchParams } = require('url')
-
 module.exports = {
   styles: [
     { href: '/dist/css/sfds.css' },
-    { href: '/dist/css/docs.css' },
-    ...[
-      {
-        name: 'Rubik',
-        weights: [300, 400, 500]
-      },
-      {
-        name: 'Noto Sans TC',
-        weights: [300, 400, 500],
-        display: 'swap',
-        subset: 'chinese-traditional'
-      },
-      {
-        name: 'Roboto Mono',
-        weights: [400],
-        display: 'swap'
-      }
-    ]
-      .map(({ name, weights = [], ...rest }) => {
-        const family = `${name}:${weights.join(',')}`
-        const query = new URLSearchParams({ family, ...rest })
-        return {
-          href: `https://fonts.googleapis.com/css?${query}`
-        }
-      })
+    { href: '/dist/css/fonts.css' },
+    { href: '/dist/css/docs.css' }
   ],
   scripts: [
     { src: '/dist/js/docs.js' },
