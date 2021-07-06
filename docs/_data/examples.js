a91753d5621785e9590099ac46d6b538f6efd95b
Shawn Allen <shawnbot@users.noreply.github.com>
2021-07-06 12:46:41 -0700

    twiddle example bits

--- docs/_data/examples.js
+++ docs/_data/examples.js
@@ -3,10 +3,11 @@ const remark = require('remark')
 const visit = require('unist-util-visit')
 const { codeExamplePlugin } = require('../../lib/remark')
 const { readFileSync } = require('fs')
+const site = require('./site')
 
 module.exports = getExamples
 
-async function getExamples () {
+async function getExamples ({ package: pkg }) {
   const examples = []
   const mds = await globby('docs/**/*.md')
   const processor = remark()
@@ -26,6 +27,28 @@ async function getExamples () {
       path,
       contents: readFileSync(path, 'utf8')
     })
+  }
+
+  const unpkgBaseURL = `https://unpkg.com/${pkg.name}@${pkg.version}`
+  const exampleFooter = `
+    <a
+      class="mt-100 block text-mono text-grey-4 p-8"
+      href="${unpkgBaseURL}/">
+      ${pkg.name}@${pkg.version}
+    </a>
+  `
+
+  for (const example of examples) {
+    example.codepen = {
+      title: `${site.title}: ${example.title}`,
+      html: `${example.code}${exampleFooter}`,
+      editors: '100',
+      js_external: `${unpkgBaseURL}/dist/js/sfds.js`,
+      css_external: [
+        `${unpkgBaseURL}/dist/css/sfds.css`,
+        `${unpkgBaseURL}/dist/css/fonts.css`
+      ].join(';')
+    }
   }
 
   return examples
