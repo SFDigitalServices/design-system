e1ba6a6aec70bec61fe6b1a20a2bbaf46c9eb67b
Shawn Allen <shawnbot@users.noreply.github.com>
2021-08-17 07:20:16 -0700

    add 11ty toc plugin, custom nav

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,16 @@
 const remark = require('@fec/eleventy-plugin-remark')
 const remarkConfig = require('./lib/remark').eleventyPluginConfig
-const navigation = require('@11ty/eleventy-navigation')
+const navigation = require('./lib/eleventy/nav')
 const yaml = require('js-yaml')
+const toc = require('eleventy-plugin-toc')
 
 module.exports = config => {
   config.addPlugin(navigation)
   config.addPlugin(remark, remarkConfig)
+  config.addPlugin(toc, {
+    tags: ['h3'],
+    ul: true
+  })
 
   config.addDataExtension('yml', contents => yaml.safeLoad(contents))
 
