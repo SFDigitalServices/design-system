5b88adc1569e2c4da1b6b61e423974dd39bb27e2
Shawn Allen <shawnbot@users.noreply.github.com>
2021-08-23 16:01:14 -0700

    better git metadata

--- docs/_data/git.js
+++ docs/_data/git.js
@@ -1,29 +1,28 @@
 const { spawnSync } = require('child_process')
 const pkg = require('../../package.json')
 
+const repoUrl = `https://github.com/${pkg.repository}`
+const {
+  HEROKU_BRANCH: branch = git('symbolic-ref', '--short', 'HEAD'),
+  HEROKU_SLUG_COMMIT: sha = git('rev-parse', '--short', 'HEAD'),
+  HEROKU_PR_NUMBER: pr
+} = process.env
+
 module.exports = {
-  repo: `https://github.com/${pkg.repository}`,
+  repo: repoUrl,
   defaultBranch: 'main',
-  get branch () {
-    try {
-      return git(['symbolic-ref', '--short', 'HEAD'])
-    } catch (error) {
-      console.warn('unable to get git branch:', error)
-      return undefined
-    }
-  },
-  get sha () {
-    try {
-      return git(['rev-parse', '--short', 'HEAD'])
-    } catch (error) {
-      console.warn('unable to get git SHA:', error)
-      return undefined
-    }
-  }
+  pr: pr ? `${repoUrl}/pull/${pr}` : null,
+  branch,
+  sha
 }
 
-function git (args, execOptions) {
-  return spawnSync('git', args, Object.assign({
-    encoding: 'utf8'
-  }, execOptions)).stdout.trim()
+function git (...args) {
+  try {
+    return spawnSync('git', args, { encoding: 'utf8' })
+      .stdout
+      .trim()
+  } catch (error) {
+    console.warn('unable to call `git %s`:', args.join(' '), error)
+    return undefined
+  }
 }
