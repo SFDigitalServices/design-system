1c99a93519bd45964014da18639c8acc59e894a3
Shawn Allen <shawnbot@users.noreply.github.com>
2021-08-23 13:54:00 -0700

    add remark plugin to interpolate standalone code examples w/nunjucks

--- docs/_data/examples.js
+++ docs/_data/examples.js
@@ -1,7 +1,8 @@
 const globby = require('globby')
 const remark = require('remark')
 const visit = require('unist-util-visit')
-const { codeExamplePlugin } = require('../../lib/remark')
+const interpolateCode = require('../../lib/remark/interpolate-code')
+const codeExample = require('../../lib/remark/code-example')
 const { readFileSync } = require('fs')
 
 module.exports = getExamples
@@ -11,7 +12,8 @@ async function getExamples () {
   const mds = await globby('docs/**/*.md')
   const processor = remark()
     .use(require('remark-slug'))
-    .use(codeExamplePlugin)
+    .use(interpolateCode)
+    .use(codeExample)
     .use(() => (tree, file) => {
       visit(tree, 'code', (node, index, parent) => {
         if (node.data?.id && node.data?.meta?.static !== 'true') {
