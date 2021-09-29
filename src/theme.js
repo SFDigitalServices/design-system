cda62fbc605b474b6456763735305fb2f76045d0
Shawn Allen <shawnbot@users.noreply.github.com>
2021-09-29 16:08:15 -0700

    add gap, grid, and margin (w/"gutter-auto") to tw theme

--- src/theme.js
+++ src/theme.js
@@ -35,6 +35,29 @@ module.exports = {
     3: '3px',
     4: '4px'
   },
+  gap: theme => ({
+    ...theme('spacing')
+  }),
+  gridTemplateColumns: {
+    1: 'repeat(1, minmax(0, 1fr))',
+    2: 'repeat(2, minmax(0, 1fr))',
+    3: 'repeat(3, minmax(0, 1fr))',
+    6: 'repeat(6, minmax(0, 1fr))',
+    12: 'repeat(12, minmax(0, 1fr))'
+  },
+  gridColumn: {
+    'span-auto': 'auto',
+    'span-1': 'span 1 / span 1',
+    'span-2': 'span 2 / span 2',
+    'span-3': 'span 3 / span 3',
+    'span-4': 'span 4 / span 4',
+    'span-6': 'span 6 / span 6',
+    'span-12': 'span 12 / span 12'
+  },
+  margin: theme => ({
+    ...theme('spacing'),
+    'gutter-auto': `calc((100vw - ${breakpoints.lg}) / 2)`
+  }),
   maxWidth: theme => ({
     ...breakpoints,
     ...theme('width')
