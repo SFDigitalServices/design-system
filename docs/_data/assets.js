a3d5fc1c28f6986bd412075c72181b318a0dd6cd
Shawn Allen <shawnbot@users.noreply.github.com>
2021-10-07 21:55:34 -0700

    replace external deps with docs.js

--- docs/_data/assets.js
+++ docs/_data/assets.js
@@ -5,8 +5,7 @@ module.exports = {
     { href: '/dist/css/docs.css' }
   ],
   scripts: [
-    { src: '/dist/js/docs.js' },
-    { src: 'https://unpkg.com/@sfgov/icons@0.0.1/dist/sfgov-icons.umd.js', defer: true },
-    { src: 'https://unpkg.com/@github/clipboard-copy-element@1.1.2/dist/index.umd.js', defer: true }
+    { src: '/dist/js/sfds.js' },
+    { src: '/dist/js/docs.js' }
   ]
 }
