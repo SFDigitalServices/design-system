b8f8d1e1c7b3c9ca5ec681cb18c130262231a332
jacksongonzales <jackson.gonzales@gmail.com>
2021-12-03 13:26:42 -0800

    inherit some babel changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,39 @@
 module.exports = {
+  parser: '@babel/eslint-parser',
   plugins: ['sfgov'],
   extends: [
-    'plugin:sfgov/recommended',
-    'plugin:sfgov/node'
+    'plugin:sfgov/recommended'
   ],
-  env: {
-    es2021: true,
-    node: true
-  },
-  parserOptions: {
-    ecmaVersion: 12
-  },
   rules: {
-  }
+  },
+  overrides: [
+    {
+      files: ['src/js/*.js', 'src/icons/index.js'],
+      env: {
+        browser: true
+      },
+      parserOptions: {
+        sourceType: 'module'
+      }
+    },
+    {
+      files: 'lib/**/*.js',
+      extends: ['plugin:sfgov/node'],
+      env: {
+        node: true
+      }
+    },
+    {
+      files: 'rollup.config.js',
+      parserOptions: {
+        sourceType: 'module'
+      }
+    },
+    {
+      files: 'scripts/*.js',
+      rules: {
+        'node/shebang': 0
+      }
+    }
+  ]
 }
