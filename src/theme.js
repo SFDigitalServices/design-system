4dc4f8ee0586bffa81a6af95eeab1b33ff581c07
Shawn Allen <shawnbot@users.noreply.github.com>
2021-12-08 17:26:08 -0500

    Add responsive container and grid styles (#49)
    * enable gap + column tailwind plugins
    
    * add 96px spacing step
    
    * [breaking] change "lg" breakpoint to "md"; add "xl"
    
    * add gap, grid, and margin (w/"gutter-auto") to tw theme
    
    * add grid.css, .responsive-grid + .responsive-container
    
    * fix title of fullscreen example link
    
    * stub out responsive grid + container examples
    
    * add back m*-auto
    
    * add "1/1" width and reorder keys
    
    * add breakpoints to "width" utilities (w-lg, etc.)
    
    * add h*-auto
    
    * add support for wrapper_class="..." in code blocks; nix margin on responsive-container example
    
    * delete hidden node dump comment from example template
    
    * better whitespace in spacing examples
    
    * delete empty "Position" heading from spacing docs
    
    * simplify theme.js, add more gridColumns and gridTemplateColumns entries
    
    * fix up .responsive-container margins
    
    * nix mx-gutter-auto utility
    
    * enable gridRow + gridTemplateRows in Tailwind
    
    * add theme specs for gridRow + gridTemplateRows
    
    * Update src/theme.js
    
    * add deprecate comments to bad spacing values
    
    * add todo column for line heights in spacing scale
    
    * nix unnecessary lg:grid-cols-12 in .responsive-grid
    
    * Revert "[breaking] change "lg" breakpoint to "md"; add "xl""
    
    This reverts commit cbd778fd96143cb1705790e39495bd3b222385f7.
    
    * feat: add new "md" breakpoint (420px)
    
    * docs(grid): add figma link, row-span utils
    
    * fix(grid): change .responsive-container max-width
    
    The max-w-lg class is preventing the responsive container from reaching its full (xl) width. It should be max-w-xl.
    
    * chore(lint): fix named default import of @rollup/plugin-babel
    
    * fix(typography): add .title-sm/-xs defaults
    
    Co-authored-by: Jackson Flint-Gonzales <jackson.gonzales@gmail.com>

--- src/theme.js
+++ src/theme.js
@@ -3,10 +3,19 @@ const { text: textColor, ...colors } = require('./tokens/colors')
 const { fontFamily, ...typography } = require('./tokens/typography')
 const spacing = require('./tokens/spacing')
 
+// available column grid templates
+const gridColumns = [1, 2, 3, 6, 12]
+// possible per-column spans
+const gridColumnSpans = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+// available row grid templates
+const gridRows = [1, 2, 3]
+// possible per-row spans
+const gridRowSpans = gridRows
+
 module.exports = {
   colors,
   screens: breakpoints,
-  spacing,
+  spacing, // TODO: add line heights @3
   fontFamily: {
     ...fontFamily,
     inherit: 'inherit'
@@ -35,26 +44,72 @@ module.exports = {
     3: '3px',
     4: '4px'
   },
+  gap: {
+    ...spacing
+  },
+  gridColumn: {
+    'span-auto': 'auto',
+    'span-full': '1 / -1',
+    ...Object.fromEntries(
+      gridColumnSpans.map(n => [
+        `span-${n}`,
+        `span ${n} / span ${n}`
+      ])
+    )
+  },
+  gridRow: {
+    'span-auto': 'auto',
+    'span-full': '1 / -1',
+    ...Object.fromEntries(
+      gridRowSpans.map(n => [
+        `span-${n}`,
+        `span ${n} / span ${n}`
+      ])
+    )
+  },
+  gridTemplateColumns: {
+    ...Object.fromEntries(
+      gridColumns.map(n => [
+        n,
+        `repeat(${n}, minmax(0, 1fr))`
+      ])
+    )
+  },
+  gridTemplateRows: {
+    ...Object.fromEntries(
+      gridRows.map(n => [
+        n,
+        `repeat(${n}, minmax(0, auto))`
+      ])
+    )
+  },
+  height: {
+    auto: 'auto',
+    full: '100%',
+    ...spacing
+  },
+  margin: {
+    auto: 'auto',
+    ...spacing
+  },
   maxWidth: theme => ({
     ...breakpoints,
     ...theme('width')
   }),
-  width: theme => ({
+  width: {
     auto: 'auto',
+    '1/1': '100%',
     '1/2': '50%',
-    '2/2': '100%',
     '1/3': '33.3333333%',
-    '2/3': '66.6666666%',
-    '3/3': '100%',
     '1/4': '25%',
+    '2/2': '100%',
+    '2/3': '66.6666666%',
     '2/4': '50%',
+    '3/3': '100%',
     '3/4': '75%',
     '4/4': '100%',
     full: '100%',
-    ...theme('spacing')
-  }),
-  height: theme => ({
-    full: '100%',
-    ...theme('spacing')
-  })
+    ...breakpoints,
+    ...spacing
+  }
 }
