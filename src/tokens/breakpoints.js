ea36a77a899945c2da792f2ac7ab494d9e707274
Shawn Allen <shawnbot@users.noreply.github.com>
2022-01-11 11:09:05 -0800

    delete source files from archive

--- src/tokens/breakpoints.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  md: '420px',
-  lg: '768px',
-  xl: '1090px'
-}
--- src/tokens/index.js
+++
@@ -1,8 +0,0 @@
-const typography = require('./typography')
-
-module.exports = {
-  breakpoints: require('./breakpoints'),
-  colors: require('./colors'),
-  spacing: require('./spacing'),
-  ...typography
-}
