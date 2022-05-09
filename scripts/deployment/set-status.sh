17c504176ca0b8707943c67e3b704d9b161dac0f
Shawn Allen <shawnbot@users.noreply.github.com>
2022-05-09 12:59:28 -0700

    fix(ci): fix up scripts/deployment/get-id.sh

--- scripts/deployment/set-status.sh
+++ scripts/deployment/set-status.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-deployment_id="$1"
-state="$2"
-app=$(scripts/heroku/get-app.sh)
+state="$1"
+deployment_id="${2:-$(scripts/deployment/get-id.sh)}"
+app="${3:-$(scripts/heroku/get-app.sh)}"
 url="https://${app}.herokuapp.com/"
 printf '{"state":"%s","environment_url":"%s","target_url":"%s"}' \
   "$state" "$url" "$url" \
