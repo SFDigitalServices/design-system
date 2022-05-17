6c2497e689d89af075e3837c4e6fb7c6bc70870f
Shawn Allen <shawnbot@users.noreply.github.com>
2022-05-17 15:37:01 -0700

    fix(docs): add docs/**/*.md? to tailwind content

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "docs/**/*.md?",
+    "src/**/*.{js,jsx,ts,tsx}",
   ],
   presets: [require("sfgov-design-system/tailwind.preset")],
 };
