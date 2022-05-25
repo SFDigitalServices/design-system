4be787f4adb9fea9136fda9281bbeaf5478a3556
Shawn Allen <shawnbot@users.noreply.github.com>
2022-05-24 21:01:56 -0700

    chore(build): write summary data in manifest.json

--- scripts/manifest.js
+++ scripts/manifest.js
@@ -3,27 +3,30 @@ const globby = require('globby')
 const { statSync } = require('fs')
 const globs = process.argv.slice(2)
 
-globby([...globs, '!**/manifest.json']).then(found => {
-  let totalSize = 0
-  const entries = found
-    .map(path => ({
-      path,
-      stat: statSync(path)
-    }))
-    .filter(item => !item.stat.isDirectory())
-    .map(({ path, stat: { size } }) => {
-      totalSize += size
-      return [path, { size }]
-    })
+// eslint-disable-next-line promise/catch-or-return
+globby([...globs, '!**/manifest.json'])
+  .then(found => {
+    let totalSize = 0
+    const entries = found
+      .map(path => ({
+        path,
+        stat: statSync(path)
+      }))
+      .filter(item => !item.stat.isDirectory())
+      .map(({ path, stat: { size } }) => {
+        totalSize += size
+        return [path, { size }]
+      })
 
-  const manifest = {
-    generated: Date.now(),
-    files: Object.fromEntries(entries),
-    summary: {
-      files: entries.length,
-      totalSize
+    const manifest = {
+      generated: Date.now(),
+      files: Object.fromEntries(entries),
+      summary: {
+        files: entries.length,
+        totalSize
+      }
     }
-  }
 
-  console.log(JSON.stringify(manifest, null, 2))
-})
+    console.log(JSON.stringify(manifest, null, 2))
+    return undefined
+  })
