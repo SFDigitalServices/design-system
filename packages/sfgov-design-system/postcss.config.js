911cbf26f1becbd62d4ffeef38a1c8f646211bdd
Shawn Allen <shawnbot@users.noreply.github.com>
2022-05-24 20:55:19 -0700

    chore(postcss): restore postcss config

--- packages/sfgov-design-system/postcss.config.js
+++ packages/sfgov-design-system/postcss.config.js
@@ -1,6 +1,16 @@
+const { NODE_ENV } = process.env
+const prod = NODE_ENV === 'production'
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {}
+  plugins: [
+    require('postcss-import'),
+    require('postcss-nested'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+    prod && require('cssnano'),
+    require('postcss-normalize-charset')
+  ].filter(Boolean),
+  map: {
+    inline: false
   }
 }
