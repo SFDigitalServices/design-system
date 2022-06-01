11dbeb6d03f59bec32d56725f8d1a66940ba8195
Corey Hunt <corey.hunt@gmail.com>
2022-06-01 18:43:30 +0000

    - Updates the styling for the inline swatch - Adds a color label to the inline swatch - Updates the mdx file to use updated inline

--- website/src/components/InlineSwatch.js
+++ website/src/components/InlineSwatch.js
@@ -2,15 +2,20 @@ import React from 'react'
 import clsx from 'clsx'
 import PropTypes from 'prop-types'
 
-export default function InlineSwatch ({ as: Comp = 'code', className, children, value = children, ...rest }) {
+export default function InlineSwatch ({ as: Comp = 'code', className, children, label, addBorder, value, ...rest }) {
   if (!isColorValue(value)) {
     return <Comp {...{ className, children, ...rest }} />
   }
   return (
-    <Comp className={clsx('inline-flex', className)} {...rest}>
-      <span className={'w-16 h-16 mr-4 block rounded-2'} style={{ backgroundColor: value }} />
-      {children}
-    </Comp>
+    <>
+      <span className='inline-block font-medium text-slate-2'>
+        {label}&nbsp;
+      </span>
+      <Comp className={clsx('inline-flex items-center px-4', className)} {...rest} style={{ borderRadius: '14px' }}>
+            <span className={`w-16 h-16 mr-4 block rounded ${addBorder ? 'border-solid border-1 border-grey-4' : ''}`} style={{ backgroundColor: value }} />
+            {value}
+      </Comp>
+    </>
   )
 }
 
@@ -20,6 +25,8 @@ InlineSwatch.propTypes = {
     PropTypes.func
   ]),
   children: PropTypes.element,
+  label: PropTypes.string.isRequired,
+  addBorder: PropTypes.bool,
   className: PropTypes.string,
   value: PropTypes.string
 }
