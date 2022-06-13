29eb5d8d40de95a948c4e1335233b301554c577c
Corey Hunt <corey.hunt@gmail.com>
2022-06-13 23:47:47 +0000

    Adds a landing page description

--- website/constants.js
+++ website/constants.js
@@ -4,6 +4,7 @@ const { repository } = defaultPackage
 const [owner, repo] = repository.split('/')
 const repoUrl = `https://github.com/${repository}`
 const treeBaseUrl = `${repoUrl}/tree/${defaultBranch}`
+const landingDesc = 'The SF.gov design system helps anyone creating digital products for San Francisco to ensure the interfaces are accessible, consistent, and efficient.'
 
 module.exports = {
   defaultPackage,
@@ -12,5 +13,6 @@ module.exports = {
   owner,
   repo,
   repoUrl,
-  treeBaseUrl
+  treeBaseUrl,
+  landingDesc
 }
