e70112eb452d3ec9d86a7620431fd67a3fb94c70
Corey Hunt <corey.hunt@gmail.com>
2022-07-14 23:09:19 +0000

    - Updates to the first RC - Fixes an import being broken by the update

--- website/src/theme/DocSidebarItem/Link/index.js
+++ website/src/theme/DocSidebarItem/Link/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import clsx from 'clsx'
-import { isActiveSidebarItem, ThemeClassNames } from '@docusaurus/theme-common'
+import { ThemeClassNames } from '@docusaurus/theme-common'
+import { isActiveSidebarItem } from '@docusaurus/theme-common/internal'
 import Link from '@docusaurus/Link'
 import isInternalUrl from '@docusaurus/isInternalUrl'
 import IconExternalLink from '@theme/Icon/ExternalLink'
