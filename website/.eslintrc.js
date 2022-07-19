a97c03026b485e4b387e49697743f79e89e42e3d
jacksongonzales <jackson.gonzales@gmail.com>
2022-07-19 10:33:43 -0700

    remove eslint-mdx for now

--- website/.eslintrc.js
+++ website/.eslintrc.js
@@ -30,4 +30,16 @@ module.exports = {
       }
     ]
   }
+  /*
+    This plugin (https://github.com/mdx-js/eslint-mdx) causes linting to hang indefinitely on .mdx files.
+    In the future, if we find markdownlint isn't meeting our linting
+    needs, we can return to attempting to debug this, or perhaps look
+    into remark-lint (https://github.com/remarkjs/remark-lint)
+  */
+  // overrides: [
+  //   {
+  //     files: ['*.md', '*.mdx'],
+  //     extends: 'plugin:mdx/recommended'
+  //   }
+  // ]
 }
