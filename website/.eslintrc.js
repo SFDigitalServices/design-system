f4bd24f508888d8b8feafc4e28d45d7dbf1dfcd0
Jackson Flint-Gonzales <jackson.gonzales@gmail.com>
2022-07-28 16:03:48 -0700

    DESSYS-217 lint md and mdx files (#100)
    * DESSYS-217 lint md and mdx files
    
    * remove eslint-mdx for now

--- website/.eslintrc.js
+++ website/.eslintrc.js
@@ -32,4 +32,16 @@ module.exports = {
       version: 'detect'
     }
   }
+  /*
+    This plugin (https://github.com/mdx-js/eslint-mdx) causes linting to hang indefinitely on .mdx files.
+    In the future, if we find markdownlint isn't meeting our linting
+    needs, we can return to attempting to debug this, or perhaps look
+    into remark-lint (https://github.com/remarkjs/remark-lint)
+  */
+  // overrides: [
+  //   {
+  //     files: ['*.md', '*.mdx'],
+  //     extends: 'plugin:mdx/recommended'
+  //   }
+  // ]
 }
