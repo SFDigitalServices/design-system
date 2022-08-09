0406aad4fa8a0ef7b89044f9c6c5560f5ccef439
Shawn Allen <shawnbot@users.noreply.github.com>
2022-08-09 13:43:33 -0700

    chore(docs): rename defaultPackage to designSystemPackage, add designSystemVersion

--- website/constants.js
+++ website/constants.js
@@ -1,13 +1,14 @@
-const defaultPackage = require('sfgov-design-system/package.json')
+const designSystemPackage = require('sfgov-design-system/package.json')
 const defaultBranch = 'main'
-const { repository } = defaultPackage
+const { repository } = designSystemPackage
 const [owner, repo] = repository.split('/')
 const repoUrl = `https://github.com/${repository}`
 const treeBaseUrl = `${repoUrl}/tree/${defaultBranch}`
 const landingDesc = 'The SF.gov design system helps anyone creating digital products for San Francisco to ensure the interfaces are accessible, consistent, and efficient.'
 
 module.exports = {
-  defaultPackage,
+  designSystemPackage,
+  designSystemVersion: process.env.SFGOV_DESIGN_SYSTEM_VERSION || designSystemPackage.version,
   defaultBranch,
   repository,
   owner,
