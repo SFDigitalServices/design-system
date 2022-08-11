0379625fae31cd9f71ce63529646feea549d03fc
Corey Hunt <corey.hunt@gmail.com>
2022-08-11 17:16:39 +0000

    Adds some non-breaking spaces to the content

--- website/constants.js
+++ website/constants.js
@@ -4,7 +4,7 @@ const { repository } = defaultPackage
 const [owner, repo] = repository.split('/')
 const repoUrl = `https://github.com/${repository}`
 const treeBaseUrl = `${repoUrl}/tree/${defaultBranch}`
-const landingDesc = 'The SF.gov design system helps anyone creating digital products for San Francisco to ensure the interfaces are accessible, consistent, and efficient.'
+const landingDesc = 'The SF.gov design system helps anyone creating digital products for San\xa0Francisco to ensure the interfaces are accessible, consistent, and\xa0efficient.'
 
 module.exports = {
   defaultPackage,
