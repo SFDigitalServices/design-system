6a8f3b7ed3ce3afec8992534811ceac9ba0fa7ba
Shawn Allen <shawnbot@users.noreply.github.com>
2022-08-22 17:11:28 -0700

    chore(docs): wrap EncapsulatedStyleRoot in browser check

--- website/src/components/EncapsulatedStyleRoot.jsx
+++ website/src/components/EncapsulatedStyleRoot.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import root from 'react-shadow'
+import useIsBrowser from '@docusaurus/useIsBrowser'
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import sfdsStyles from '!!raw-loader!sfgov-design-system/dist/css/sfds.css'
 
 export default function EncapsulatedStyleRoot ({ children, ...props }) {
-  return (
-    <root.div {...props}>
-      <style>
-        {sfdsStyles}
-      </style>
-      {children}
-    </root.div>
-  )
+  const isBrowser = useIsBrowser()
+  if (isBrowser) {
+    return (
+      <root.div {...props}>
+        <style>
+          {sfdsStyles}
+        </style>
+        {children}
+      </root.div>
+    )
+  } else {
+    return children
+  }
 }
