8ee3cb7d8fdd715c86160c1afcbfc284574cc82f
Shawn Allen <shawnbot@users.noreply.github.com>
2022-08-24 16:15:26 -0700

    fix(docs): rework SFGovIconWrapper to re-render properly

--- website/src/components/SFGovIcon.js
+++ website/src/components/SFGovIcon.js
@@ -1,12 +1,18 @@
-import React, { useMemo } from 'react'
+import React, { useLayoutEffect, useRef } from 'react'
+import useIsBrowser from '@docusaurus/useIsBrowser'
 
 /**
- * This is a workaround for re-rendered custom elements that ignores children
- * and memoizes the return value.
- * 
- * @param {React.PropsWithoutRef} props 
- * @returns {JSX.Element}
+ * This is a workaround for custom elements that get their
+ * <svg> contents blown away by React. We use a callback in
+ * useLayoutEffect() to force the SFGovIcon instance to re-render.
  */
-export default function SFGovIcon ({ children, ...props }) {
-  return useMemo(() => (<sfgov-icon {...props} />), [props])
+export default function SFGovIconWrapper (props) {
+  const ref = useRef()
+  const isBrowser = useIsBrowser()
+  useLayoutEffect(() => {
+    if (isBrowser && ref.current && window?.customElements?.get('sfgov-icon')) {
+      ref.current.render()
+    }
+  }, [isBrowser, ref, props])
+  return <sfgov-icon ref={ref} {...props} />
 }
