d57ed8abd9083099cdcbf15b5e25f6b0801effa7
Shawn Allen <shawnbot@users.noreply.github.com>
2022-08-24 17:54:05 -0700

    chore(lint): fix storybook linting

---
+++ storybook/babel.config.js
@@ -0,0 +1,7 @@
+/** @type {import('@babel/core').PartialConfig} */
+module.exports = {
+  presets: [
+    '@babel/preset-react'
+  ],
+  sourceType: 'unambiguous'
+}
--- storybook/stories/Header.jsx
+++ storybook/stories/Header.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'
+import PropTypes from 'prop-types'
 
-import { Button } from './Button';
-import './header.css';
+import { Button } from './Button'
+import './header.css'
 
 export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
   <header>
@@ -27,31 +27,35 @@ export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
         <h1>Acme</h1>
       </div>
       <div>
-        {user ? (
+        {user
+          ? (
           <>
             <span className="welcome">
               Welcome, <b>{user.name}</b>!
             </span>
             <Button size="small" onClick={onLogout} label="Log out" />
           </>
-        ) : (
+            )
+          : (
           <>
             <Button size="small" onClick={onLogin} label="Log in" />
             <Button primary size="small" onClick={onCreateAccount} label="Sign up" />
           </>
-        )}
+            )}
       </div>
     </div>
   </header>
-);
+)
 
 Header.propTypes = {
-  user: PropTypes.shape({}),
+  user: PropTypes.shape({
+    name: PropTypes.string
+  }).isRequired,
   onLogin: PropTypes.func.isRequired,
   onLogout: PropTypes.func.isRequired,
-  onCreateAccount: PropTypes.func.isRequired,
-};
+  onCreateAccount: PropTypes.func.isRequired
+}
 
 Header.defaultProps = {
-  user: null,
-};
+  user: null
+}
