fb62dfd2604ac7181639f742391f6f1985bd81c8
Shawn Allen <shawnbot@users.noreply.github.com>
2022-08-24 17:19:09 -0700

    feat(storybook): bootstrap storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,7 @@
 module.exports = {
   parser: '@babel/eslint-parser',
-  plugins: [
-    'sfgov',
-    'unicorn'
-  ],
-  extends: [
-    'plugin:sfgov/recommended'
-  ],
+  plugins: ['sfgov', 'unicorn'],
+  extends: ['plugin:sfgov/recommended', 'plugin:storybook/recommended'],
   rules: {
     'unicorn/expiring-todo-comments': ['error', {
       allowWarningComments: true
@@ -14,39 +9,35 @@ module.exports = {
     'no-trailing-spaces': ['warn', {
       ignoreComments: true
     }],
-    'padding-line-between-statements': [
-      'warn',
-      { blankLine: 'always', prev: '*', next: 'function' }
-    ]
+    'padding-line-between-statements': ['warn', {
+      blankLine: 'always',
+      prev: '*',
+      next: 'function'
+    }]
   },
-  overrides: [
-    {
-      files: ['src/**/*.js'],
-      env: {
-        browser: true
-      },
-      parserOptions: {
-        sourceType: 'module'
-      }
+  overrides: [{
+    files: ['src/**/*.js'],
+    env: {
+      browser: true
     },
-    {
-      files: 'lib/**/*.js',
-      extends: ['plugin:sfgov/node'],
-      env: {
-        node: true
-      }
-    },
-    {
-      files: 'rollup.config.js',
-      parserOptions: {
-        sourceType: 'module'
-      }
-    },
-    {
-      files: '**/scripts/*.js',
-      rules: {
-        'node/shebang': 0
-      }
+    parserOptions: {
+      sourceType: 'module'
     }
-  ]
-}
+  }, {
+    files: 'lib/**/*.js',
+    extends: ['plugin:sfgov/node'],
+    env: {
+      node: true
+    }
+  }, {
+    files: 'rollup.config.js',
+    parserOptions: {
+      sourceType: 'module'
+    }
+  }, {
+    files: '**/scripts/*.js',
+    rules: {
+      'node/shebang': 0
+    }
+  }]
+};
