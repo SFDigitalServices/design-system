bfcf905f58c538122e2279afee5e7f400b7b7e1f
Shawn Allen <shawnbot@users.noreply.github.com>
2022-08-25 00:15:27 -0700

    chore(storybook): lint .storybook, add <base href=/storybook/>

--- storybook/.storybook/main.js
+++ storybook/.storybook/main.js
@@ -1,15 +1,21 @@
+/** @type {import('@storybook/core-common').StorybookConfig} */
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions'
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5'
+  },
+  managerHead (head, { configType }) {
+    return configType === 'PRODUCTION'
+      ? `${head}<base href="/storybook/">`
+      : head
   }
 }
