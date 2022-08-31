42ab8a5f15dafecf669af1976e09e3d001e11709
Shawn Allen <shawnbot@users.noreply.github.com>
2022-08-31 14:04:13 -0700

    chore(docs): un-swizzle MDXContent

--- website/src/theme/MDXContent/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import MDXContent from '@theme-original/MDXContent'
-
-export default function MDXContentWrapper (props) {
-  // TODO: add components={ourComponents}?
-  return (
-    <MDXContent {...props} />
-  )
-}
