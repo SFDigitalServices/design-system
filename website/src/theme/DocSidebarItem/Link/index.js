95d4ea99c269fbe7fa7cc038213d91ed4c277e1c
Shawn Allen <shawnbot@users.noreply.github.com>
2022-09-30 14:38:44 -0700

    chore(lint): remove unused disables

--- website/src/theme/DocSidebarItem/Link/index.js
+++ website/src/theme/DocSidebarItem/Link/index.js
@@ -11,7 +11,7 @@ export default function DocSidebarItemLink ({
   onItemClick,
   activePath,
   level,
-  index, // eslint-disable-line no-unused-vars
+  index,
   ...props
 }) {
   const { href, label, className } = item
