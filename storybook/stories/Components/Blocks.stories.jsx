80aeb5c57b645ddc6a631d2aba9327ec62e336ae
Shawn Allen <shawnbot@users.noreply.github.com>
2022-10-05 13:07:15 -0700

    chore: stub out Blocks component group

---
+++ storybook/stories/Components/Blocks.stories.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { withClasses } from '../../src/utils'
+
+/** @type {import('@storybook/addons').StoryContext} */
+export default {
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/nCDNClTAztpLol9l74QWSP/Design-System-Components?node-id=348%3A55'
+    }
+  }
+}
+
+export const PageTitle = () => 'TODO: Page title'
+
+PageTitle.parameters = {
+  design: {
+    url: 'https://www.figma.com/file/nCDNClTAztpLol9l74QWSP/Design-System-Components?node-id=348%3A55'
+  }
+}
+
+export const CampaignBlock = () => 'TODO: Campaign block'
+
+CampaignBlock.parameters = {
+  design: {
+    url: 'https://www.figma.com/file/nCDNClTAztpLol9l74QWSP/Design-System-Components?node-id=3185%3A0'
+  }
+}
+
+export const PrimarySpotlight = () => 'TODO: Primary spotlight'
+
+PrimarySpotlight.parameters = {
+  design: {
+    url: 'https://www.figma.com/file/nCDNClTAztpLol9l74QWSP/Design-System-Components?node-id=3185%3A0'
+  }
+}
+
+export const SecondarySpotlight = () => 'TODO: Secondary spotlight'
+
+SecondarySpotlight.parameters = {
+  design: {
+    url: 'https://www.figma.com/file/nCDNClTAztpLol9l74QWSP/Design-System-Components?node-id=3185%3A0'
+  }
+}
