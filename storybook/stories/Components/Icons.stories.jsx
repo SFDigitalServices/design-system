e818e8dd5acb8402dec17e5eeb90a9be7ab02e0f
Shawn Allen <shawnbot@users.noreply.github.com>
2022-10-06 15:45:41 -0700

    feat: tweak Icon story, add AllIcons

--- storybook/stories/Components/Icons.stories.jsx
+++ storybook/stories/Components/Icons.stories.jsx
@@ -1,43 +1,61 @@
-import React from 'react'
 import IconComponent, { symbols } from '../../src/Icon'
 
 /** @type {import('@storybook/addons').StoryContext} */
 export default {
-  component: IconComponent,
-  decorators: [
-    withMultipleSizes
-  ],
   args: {
     symbol: 'info'
   },
   argTypes: {
-    symbol: {
-      options: symbols,
+    size: {
+      name: 'Size',
+      type: 'number',
+      description: 'Width and height, in pixels',
+      defaultValue: 20,
       control: {
-        type: 'select'
+        type: 'number',
+        min: 16,
+        max: 64
       }
     }
   },
   parameters: {
+    controls: {
+      expanded: true
+    },
     design: {
       type: 'figma',
       url: 'https://www.figma.com/file/P0usvPQSAKOzqcH5mPFf1u/Assets?node-id=38%3A8'
-    },
-    sizes: [
-      20,
-      40,
-      64,
-      96
-    ]
+    }
   }
 }
 
 /** @type {import('@storybook/react').Story} */
-export const Icon = (args, context) => <IconComponent {...context} {...args} />
+export const Icon = IconComponent.bind({})
 
-/** @type {import('@storybook/addons').DecoratorFunction} */
-function withMultipleSizes (Story, { parameters: { sizes } }) {
-  return <div className='flex items-start gap-20'>
-    {sizes.map(size => <Story key={size} size={size} />)}
-  </div>
+Icon.argTypes = {
+  symbol: {
+    name: 'Icon',
+    description: 'The predefined symbol name',
+    type: 'string',
+    options: symbols,
+    control: {
+      type: 'select'
+    }
+  }
 }
+
+/** @type {import('@storybook/react').Story} */
+export const AllIcons = args => (
+  <table style={{ borderCollapse: 'collapse' }}>
+    <tbody>
+      {symbols.map(symbol => (
+        <tr key={symbol}>
+          <th scope='row' className='font-normal font-mono text-left'>{symbol}</th>
+          <td className='p-16'>
+            <IconComponent {...args } symbol={symbol} />
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+)
