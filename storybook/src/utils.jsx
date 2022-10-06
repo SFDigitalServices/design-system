eb85fa6751dd2326f3e751deff0d80304e58b50f
Shawn Allen <shawnbot@users.noreply.github.com>
2022-10-06 12:52:08 -0700

    chore: storybook utils updates

--- storybook/src/utils.jsx
+++ storybook/src/utils.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import clsx from 'clsx'
 
+/**
+ * Merge the `className` and `style` keys of one or more prop objects.
+ * 
+ * @param  {...{ className: import('clsx').ClassValue, style: Record<string, any> }} items
+ * @returns {{ className: string, style: Record<string, any>, [string]: any }}
+ */
 export function mergeProps (...items) {
   const props = {}
   const filtered = items.filter(Boolean)
@@ -17,10 +23,58 @@ export function mergeProps (...items) {
   })
 }
 
-export function withClasses (Component, ...classes) {
-  return (props, context) => <Component {...mergeProps(props, { className: clsx(...classes, context?.className) })} />
+/**
+ * Wrap a component in such a way that the received `className` prop merges the
+ * provided static classes with the render-time `className` prop so that:
+ * 
+ * ```jsx
+ * const Foo = withClasses('div', 'foo')
+ * <Foo /> → <div className='foo'
+ * <Foo className='bar' /> → <div className='foo bar' />
+ * ```
+ *
+ * @param {React.FC} Component 
+ * @param  {import('clsx').ClassValue[]} classes 
+ * @returns {React.FC}
+ */
+export const withClasses = (Component, ...classes) => {
+  return withPropsTransform(Component, (props, context) => {
+    return mergeProps(props, { className: clsx(...classes, context?.className) })
+  })
 }
 
+/**
+ * Wrap a component in such a way that the input props will be transformed before
+ * being passed to the underlying implementation. This is helpful for extracting
+ * transient props that shouldn't be passed as DOM attributes.
+ * 
+ * @param {React.FC} Component 
+ * @param {Function<React.PropsWithChildren, React.PropsWithChildren>} transform 
+ * @returns {React.FC}
+ */
+export function withPropsTransform (Component, transform) {
+  return function ComponentWithPropsTransform (props) {
+    return <Component {...transform(props)} />
+  }
+}
+
+/**
+ * @param {React.FC<React.PropsWithChildren & { content: React.ReactElement }>} Component 
+ * @returns {React.FC<Omit<React.PropsWithChildren, 'content'>>}
+ */
+export function withContentProp (Component) {
+  return withPropsTransform(Component, ({ content, children, ...rest }) => ({
+    children: content || children,
+    ...rest
+  }))
+}
+
+/**
+ * 
+ * @param {Record<string, { $label: string, [string]: any }>} variants 
+ * @param {Record<string, any>?} argType 
+ * @returns 
+ */
 export function variantArgType (variants, argType) {
   const { control, ...rest } = argType || {}
   return {
@@ -37,12 +91,20 @@ export function variantArgType (variants, argType) {
   }
 }
 
+/**
+ * 
+ * @param {React.FC} Component 
+ * @param {Record<string, React.PropsWithoutRef>} variants 
+ * @param {string} defaultVariant 
+ * @returns {import('@storybook/react').Story}
+ */
 export function withVariants (Component, variants, defaultVariant = 'default') {
+  /** @type {import('@storybook/react').Story} */
   function VariantComponent ({ variant, ...rest }) {
-    const { $label, ... variantProps } = variants[variant || defaultVariant]
+    const { $label, ...variantProps } = variants[variant || defaultVariant]
     return <Component {...mergeProps(variantProps, rest)} />
   }
-  
+
   VariantComponent.args = {
     variant: defaultVariant,
     ...Component.args
