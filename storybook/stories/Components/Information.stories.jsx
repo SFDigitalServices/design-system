f37e0834ec6a2027a69ccd8074b7506ade986b7a
Shawn Allen <shawnbot@users.noreply.github.com>
2022-10-06 23:29:21 -0700

    feat: stub out Icon + Information lockups

---
+++ storybook/stories/Components/Information.stories.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+/** @type {import('@storybook/addons').StoryContext} */
+export default {
+  title: 'Components / Icon and Information Lockup',
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/nCDNClTAztpLol9l74QWSP/Design-System-Components?node-id=698%3A187'
+    }
+  }
+}
+
+/** @type {import('@storybook/react').Story} */
+export const EmailAddress = () => 'TODO'
+
+/** @type {import('@storybook/react').Story} */
+export const PhoneNumbers = () => 'TODO'
+
+/** @type {import('@storybook/react').Story} */
+export const MailingAddress = () => 'TODO'
+
+/** @type {import('@storybook/react').Story} */
+export const DocumentLink = () => 'TODO'
+
+/** @type {import('@storybook/react').Story} */
+export const Timestamp = () => 'TODO'
