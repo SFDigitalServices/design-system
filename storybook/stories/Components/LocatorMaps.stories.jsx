bc9684b759a1975f26db696db9111f3cc5568679
Shawn Allen <shawnbot@users.noreply.github.com>
2022-10-06 23:35:06 -0700

    feat: stub out Locator Maps stories

---
+++ storybook/stories/Components/LocatorMaps.stories.jsx
@@ -0,0 +1,17 @@
+// import React from 'react'
+
+/** @type {import('@storybook/addons').StoryContext} */
+export default {
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/nCDNClTAztpLol9l74QWSP/Design-System-Components?node-id=3554%3A1289'
+    }
+  }
+}
+
+/** @type {import('@storybook/react').Story} */
+export const MapView = () => 'TODO'
+
+/** @type {import('@storybook/react').Story} */
+export const AddressWithLocatorMap = () => 'TODO'
