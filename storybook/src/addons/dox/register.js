28fc9b28252e82bc2df3ee79dd69386c32d0d400
Shawn Allen <shawnbot@users.noreply.github.com>
2022-10-19 15:35:43 -0700

    chore(storybook): refactor "dox" addon to augment source links

--- storybook/src/addons/dox/register.js
+++ storybook/src/addons/dox/register.js
@@ -1,9 +1,16 @@
-import React from 'react'
 import { addons, types } from '@storybook/addons'
-import { AddonPanel, Code } from '@storybook/components'
 import { components as allComponents } from '../../../../website/components'
-import { getDocsBaseUrl } from '../../utils'
 import minimatch from 'minimatch'
+
+/**
+ * This Storybook addon doesn't expose a UI; all it does is cross-reference
+ * the current path of the current story with the components index and, if
+ * a matching component is found, adds the `sourceLink` and `sourceLinkPrefix`
+ * parameters so that the Source Link addon links to the component docs.
+ * <https://storybook.js.org/addons/storybook-source-link#component-level>
+ */
+
+const ADDON_ID = 'sourceLinkConnect'
 
 /**
  * @typedef {import('../../../../website/components').ComponentMeta} ComponentMeta
@@ -13,29 +20,24 @@ import minimatch from 'minimatch'
  * } StoryOrGroup
  */
 
-const ADDON_ID = 'dox'
-const PANEL_ID = `${ADDON_ID}/panel`
-
-addons.register(ADDON_ID, (api) => {
+addons.register(ADDON_ID, api => {
   const components = allComponents.filter(comp => comp.story)
 
-  addons.add(PANEL_ID, {
+  addons.add(ADDON_ID, {
     type: types.PANEL,
-    title: 'Docs',
+    // XXX this effectively hides the tab because the empty tab button has no
+    // clickable surface area
+    title: '',
     match: () => true,
-    render: ({ active, key }) => {
-      const story = api.getCurrentStoryData()
-      const component = getComponentFromStory(story)
-      return (
-        <AddonPanel active={active && story} key={key}>
-          <div style={{ padding: '15px' }}>
-            {component
-              ? <ComponentDocs component={component} story={story} />
-              : <NoComponentDocs story={story} />
-            }
-          </div>
-        </AddonPanel>
-      )
+    render: () => {
+      const Story = api.getCurrentStoryData()
+      if (!Story) return null
+      const comp = getComponentFromStory(Story)
+      if (comp || comp.parameters?.sourceLink) return null
+
+      if (!Story.parameters) Story.parameters = {}
+      Story.parameters.sourceLink = comp.href
+      return null
     }
   })
 
@@ -50,32 +52,5 @@ addons.register(ADDON_ID, (api) => {
           .some(path => path && minimatch(path, comp.story.path))
       })
       : undefined
-  }
-
-  /**
-   * 
-   * @param {{ component: ComponentMeta, story: StoryOrGroup }} props
-   */
-  function ComponentDocs ({ component, story }) {
-    const href = `${getDocsBaseUrl()}${component.href}`
-    return <>
-      See the <a href={href}>{component.name} documentation</a> for more info.
-    </>
-  }
-
-  /**
-   * @param {{ story: StoryOrGroup }} props
-   * @returns {JSX.Element}
-   */
-  function NoComponentDocs ({ story }) {
-    const restartMessage = process.env.NODE_ENV === 'production' ? '' : ', then restart Storybook if developing locally'
-    return story
-      ? <>
-      <b>No docs are linked to this story.</b>
-      <br /><br />
-      Set one of the component definitions&rsquo; <Code>story.path</Code> to <Code>{story.id}</Code> or <Code>{story.componentId}</Code>
-      in <Code>website/components.js</Code> to link it{restartMessage}.
-    </>
-      : null
   }
 })
