6455c35197168f20d86b7199f475b63199d184bf
Shawn Allen <shawnbot@users.noreply.github.com>
2022-11-28 10:57:32 -0500

    chore(storybook): refactor preview config with theme

--- storybook/.storybook/preview.jsx
+++ storybook/.storybook/preview.jsx
@@ -1,14 +1,37 @@
-import { addParameters } from '@storybook/react'
-import { withClasses, getDocsBaseUrl } from '../src/utils'
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
+import { addParameters, addDecorator } from '@storybook/react'
+import { Box, ThemeProvider, breakpointsByName } from '@sfgov/react'
+import { getDocsBaseUrl } from '../src/utils'
 
-/**
- * These are styles applied only to the preview panel.
- */
-import 'sfgov-design-system'
-import 'sfgov-design-system/dist/css/sfds.css'
 import 'sfgov-design-system/dist/css/fonts.css'
+import 'sfgov-design-system/dist/css/sfds.css'
+
+const breakpoints = Object.fromEntries(
+  Object.entries(breakpointsByName).map(([name, width]) => [name, {
+    name: `Breakpoint: ${name}`,
+    styles: {
+      width,
+      height: '640px'
+    }
+  }])
+)
+
+addDecorator((Story, { parameters }) => (
+  <ThemeProvider>
+    <Box p={20} {...parameters.container}>
+      {Story()}
+    </Box>
+  </ThemeProvider>
+))
 
 addParameters({
+  layout: 'fullscreen',
+  viewport: {
+    viewports: {
+      ...breakpoints,
+      ...INITIAL_VIEWPORTS
+    }
+  },
   design: {
     name: 'Figma',
     type: 'figma'
@@ -17,18 +40,6 @@ addParameters({
   previewTabs: {
     'storybook/docs/panel': {
       hidden: true
-    }
-  },
-  docs: {
-    components: {
-      wrapper: withClasses('div', 'text-body responsive-container'),
-      h1: withClasses('h1', 'title-lg mt-0 mb-24'),
-      h2: withClasses('h2', 'title-md mt-0 mb-24'),
-      p: withClasses('p', 'my-24'),
-      ol: withClasses('ol', 'my-24 pl-24 space-y-8 text-inherit'),
-      ul: withClasses('ul', 'my-24 pl-24 space-y-8 text-inherit'),
-      li: withClasses('li', 'm-0 p-0 text-inherit'),
-      a: withClasses('a', '')
     }
   },
   options: {
