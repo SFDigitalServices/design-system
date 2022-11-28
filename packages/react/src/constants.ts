f60bc9b268dc0dcb5b24e1336899af2e6364c3d0
Shawn Allen <shawnbot@users.noreply.github.com>
2022-11-28 10:46:15 -0500

    chore: stub out hover, focus, hocus constants

--- packages/react/src/constants.ts
+++ packages/react/src/constants.ts
@@ -1,1 +1,7 @@
-export const HOCUS = '&:hover, &:focus'
+export const HOVER_CLASS = '__hover__'
+export const FOCUS_CLASS = '__focus__'
+export const HOCUS_CLASS = '__hocus__'
+
+export const HOVER = `&:hover, &.${HOVER_CLASS}`
+export const FOCUS = `&:focus, &.${FOCUS_CLASS}`
+export const HOCUS = `&:hover, &:focus, &.${HOCUS_CLASS}`
