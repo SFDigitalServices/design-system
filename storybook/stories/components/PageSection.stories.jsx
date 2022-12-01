780984fae0f6e7bc58718aceb51d574e1b9189c0
Jackson Flint-Gonzales <jackson.gonzales@gmail.com>
2022-11-30 16:04:17 -0800

    add contact and about sections (#138)

--- storybook/stories/components/PageSection.stories.jsx
+++ storybook/stories/components/PageSection.stories.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Section from '../../src/Section'
 import SectionTitle from '../../src/SectionTitle'
 import { ContentTile, NewsTile, TileSection } from '../../src/Tile'
+import { ContactSection as ContactSectionComponent } from '../../src/ContactSection'
 
 const imgSrc = 'https://sf.gov/sites/default/files/styles/default/public/2021-10/san-francisco-street-day-time.jpg?itok=5QqBW-AZ'
 
@@ -33,3 +34,24 @@ export const ServicesSection = args =>
   </Section>
 
 ServicesSection.args = { title: 'Services' }
+
+export const ContactSection = args => <ContactSectionComponent {...args} />
+
+ContactSection.args = {
+  address: {
+    title: 'City Administrator',
+    line1: '1 Dr. Carlton B. Goodlett Place',
+    line2: 'City Hall Room 362',
+    locality: 'San Francisco',
+    administrative_area: 'CA',
+    postal_code: '94102'
+  },
+  phone: [{
+    owner: '',
+    tel: '415-554-4851'
+  }],
+  email: {
+    owner: '',
+    email: 'city.administrator@sfgov.org'
+  }
+}
