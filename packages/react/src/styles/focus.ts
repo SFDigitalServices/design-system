2c9987fbdf8f4fd33fc14cf87d8c976a6efd90c8
Shawn Allen <shawnbot@users.noreply.github.com>
2022-12-02 10:35:49 -0800

    chore: remove unused code

--- packages/react/src/styles/focus.ts
+++
@@ -1,8 +0,0 @@
-import { ThemeUICSSObject } from 'theme-ui'
-
-export const focusStyles: ThemeUICSSObject = {
-  outlineWidth: 2,
-  outlineOffset: 2,
-  outlineStyle: 'dotted',
-  outlineColor: 'grey.4'
-}
