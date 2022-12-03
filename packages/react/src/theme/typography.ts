0154bd81b878de241b83b1c730874561c7e3b67f
Shawn Allen <shawnbot@users.noreply.github.com>
2022-12-02 16:38:08 -0800

    feat(components): add Text component

--- packages/react/src/theme/typography.ts
+++ packages/react/src/theme/typography.ts
@@ -1,16 +1,18 @@
-export const RUBIK_SANS = 'Rubik, ui-sans-serif, sans-serif'
-export const ROBOTO_MONO = 'Roboto mono, ui-monospace, monospace'
+import { CSS } from '@stitches/react'
 
 export const fonts = {
-  rubik: RUBIK_SANS,
-  roboto: ROBOTO_MONO,
+  rubik: 'Rubik, ui-sans-serif, sans-serif',
+  roboto: 'Roboto mono, ui-monospace, monospace',
+  noto: 'Noto Sans TC, ui-sans-serif, sans-serif',
   body: '$rubik',
+  chinese: '$noto',
   monospace: '$roboto'
 }
 
-export const textStyles = {
+export const textStyles: Record<string, CSS> = {
   body: {
     fontSize: '16px',
+    fontWeight: '$normal',
     lineHeight: '24px'
   },
   small: {
@@ -19,65 +21,79 @@ export const textStyles = {
   },
   bigDesc: {
     fontSize: '20px',
+    fontWeight: '$bold',
     lineHeight: '28px'
   },
   bigDescDesktop: {
     fontSize: '24px',
+    fontWeight: '$bold',
     lineHeight: '32px'
   },
   titleXs: {
     fontSize: '20px',
+    fontWeight: '$bold',
     lineHeight: '24px'
   },
   titleSm: {
     fontSize: '24px',
+    fontWeight: '$bold',
     lineHeight: '28px'
   },
   titleMd: {
     fontSize: '24px',
+    fontWeight: '$bold',
     lineHeight: '28px'
   },
   titleLg: {
     fontSize: '28px',
+    fontWeight: '$bold',
     lineHeight: '32px',
     letterSpacing: '-1px'
   },
   titleXl: {
     fontSize: '32px',
+    fontWeight: '$bold',
     lineHeight: '36px',
     letterSpacing: '-1px'
   },
   displaySm: {
     fontSize: '36px',
+    fontWeight: '$light',
     lineHeight: '40px',
     letterSpacing: '-1px'
   },
   displayLg: {
     fontSize: '44px',
+    fontWeight: '$light',
     lineHeight: '48px',
     letterSpacing: '-1px'
   },
   titleMdDesktop: {
     fontSize: '32px',
+    fontWeight: '$bold',
     lineHeight: '36px'
   },
   titleLgDesktop: {
     fontSize: '44px',
+    fontWeight: '$bold',
     lineHeight: '52px',
     letterSpacing: '-1px'
   },
   titleXlDesktop: {
     fontSize: '60px',
+    fontWeight: '$bold',
     lineHeight: '64px',
     letterSpacing: '-1px'
   },
   displaySmDesktop: {
     fontSize: '48px',
+    fontWeight: '$light',
     lineHeight: '52px',
     letterSpacing: '-1px'
   },
   displayLgDesktop: {
     fontSize: '72px',
+    fontWeight: '$light',
     lineHeight: '76px',
     letterSpacing: '-2px'
   }
@@ -86,16 +102,16 @@ export const textStyles = {
 export const fontWeights = {
   light: 300,
   normal: 400,
-  bold: 600
+  bold: 600,
+  boldChinese: 500,
+  ...collect('fontWeight')
 }
 
 export const fontSizes = collect('fontSize')
 export const lineHeights = collect('lineHeight')
 export const letterSpacings = collect('letterSpacing')
 
-Object.assign(fontWeights, collect('fontWeight'))
-
-function collect (prop: string) {
+function collect (prop: keyof CSS) {
   const map = {}
   for (const [name, style] of Object.entries(textStyles)) {
     if (prop in style) {
