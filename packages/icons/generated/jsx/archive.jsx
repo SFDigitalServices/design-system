92615b11f0a097be311344f90841fee886178813
Shawn Allen <shawnbot@users.noreply.github.com>
2022-12-05 15:53:15 -0800

    chore: add generated SVG + React icons

---
+++ packages/icons/generated/jsx/archive.jsx
@@ -0,0 +1,3 @@
+import * as React from 'react'
+const Archive = props => <svg viewBox="0 0 20 20" {...props}><path fillRule="evenodd" clipRule="evenodd" d="M2.5 5.8125V3.1875H17.5V5.8125H2.5ZM0 1.875C0 1.39175 0.447715 1 1 1H19C19.5523 1 20 1.39175 20 1.875V7.125C20 7.60825 19.5523 8 19 8H18V18.1429C18 18.6162 17.5523 19 17 19H3C2.44772 19 2 18.6162 2 18.1429V8H1C0.447716 8 0 7.60825 0 7.125V1.875ZM4.5 8V16.8571H15.5V8H4.5Z" /><rect x={7} y={9} width={6} height={2} rx={1} /></svg>
+export default Archive
