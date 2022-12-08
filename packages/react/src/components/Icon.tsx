49d015d52f5f54db9cc2b3bfad4ac6d54e197d16
Shawn Allen <shawnbot@users.noreply.github.com>
2022-12-07 21:33:42 -0800

    chore(react): simplify Icon component

--- packages/react/src/components/Icon.tsx
+++ packages/react/src/components/Icon.tsx
@@ -1,29 +1,13 @@
-/* eslint-disable import/namespace */
-import React, { ComponentPropsWithoutRef, ComponentType } from 'react'
-import * as components from '@sfgov/icons'
-import { components as iconsIndex } from '@sfgov/icons/generated/index.json'
+import React, { ComponentProps, ComponentType } from 'react'
 import { styled } from '../stitches.config'
 
-export type SVGProps = ComponentPropsWithoutRef<'svg'>
-export type IconName = keyof typeof iconsIndex
-
-export { iconsIndex }
-
-export const iconsByName = Object.fromEntries(
-  Object.entries(iconsIndex)
-    .map(([id, { component }]) => [id, components[component]])
-) as Record<IconName, ComponentType<SVGProps>>
-
-function getComponent (name: IconName | string): ComponentType<SVGProps> {
-  return iconsByName[name] || 'svg'
-}
-
+export type SVGProps = ComponentProps<'svg'>
 export type IconProps = SVGProps & {
-  symbol: IconName
+  symbol: ComponentType<SVGProps>
 }
 
 export const Icon = styled(function IconImpl ({ symbol, ...rest }: IconProps) {
-  const Component = getComponent(symbol)
+  const Component = symbol || 'svg'
   return <Component {...rest} />
 }, {
   fill: 'currentcolor',
