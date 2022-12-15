b6269bce3a69ec1b1fe3e4763647b05ba0544c22
Shawn Allen <shawnbot@users.noreply.github.com>
2022-12-14 23:47:04 -0800

    chore(css): build out global + SSR style support

--- packages/react/src/components/GlobalStyle.tsx
+++ packages/react/src/components/GlobalStyle.tsx
@@ -1,17 +1,35 @@
-import { CSS, globalCss } from '../stitches.config'
+import React, { ComponentProps } from 'react'
 
-export function GlobalStyle (css?: CSS) {
-  globalCss({
-    '@import': `url('${getFontsURL({
-      Rubik: 'wght@300;400;600',
-      'Noto Sans TC': 'wght@300;400;500',
-      'Roboto Mono': 'wght@400'
-    })}')`
-  })
-  return null
+export type GoogleFontsSpec = Record<string, string>
+
+export type GlobalCSSProps = {
+  googleFonts?: GoogleFontsSpec
 }
 
-function getFontsURL (fonts: Record<string, string | null>) {
+export type GlobalStyleProps = Omit<ComponentProps<'style'>, 'dangerouslySetInnerHTML'> & GlobalCSSProps
+
+export const DEFAULT_GOOGLE_FONTS: GoogleFontsSpec = {
+  Rubik: 'wght@300;400;600',
+  'Noto Sans TC': 'wght@300;400;500',
+  'Roboto Mono': 'wght@400'
+}
+
+export function getDefaultGlobalCss ({ googleFonts = DEFAULT_GOOGLE_FONTS }: GlobalCSSProps) {
+  return `
+    @import url('${getGoogleFontsURL(googleFonts)}');
+  `.trim()
+}
+
+export function GlobalStyle ({ googleFonts = DEFAULT_GOOGLE_FONTS, ...rest }: GlobalStyleProps) {
+  return <style id='sfgov-global-css' dangerouslySetInnerHTML={{
+    __html: getDefaultGlobalCss({ googleFonts })
+  }} />
+}
+
+export function getGoogleFontsURL (fonts: GoogleFontsSpec = DEFAULT_GOOGLE_FONTS) {
+  if (!fonts || typeof fonts !== 'object' || Array.isArray(fonts)) {
+    throw new Error(`Expected a Google Fonts object spec, but got ${typeof fonts}`)
+  }
   const families = Object.entries(fonts)
     .map(([family, args]) => `family=${escape(family)}${args ? `:${args}` : ''}`)
     .join('&')
--- packages/react/src/components/SSRStyle.tsx
+++ packages/react/src/components/SSRStyle.tsx
@@ -1,10 +1,33 @@
 import React, { ComponentProps } from 'react'
+import { GlobalCSSProps, getGoogleFontsURL, DEFAULT_GOOGLE_FONTS } from './GlobalStyle'
 import { getCssText, reset } from '../stitches.config'
 
-export type SSRStyleProps = Omit<ComponentProps<'style'>, 'dangerouslySetInnerHTML'>
+export type SSRStyleProps = Omit<ComponentProps<'style'>, 'dangerouslySetInnerHTML'> & GlobalCSSProps
 
-export function SSRStyle (props: SSRStyleProps) {
+/**
+ * The SSR (Server Side Render) styles component is necessary for avoiding
+ * the FOUC (Flash of Unstyled Content) when rendering server-side then
+ * hydrating on the client. It does a couple of things:
+ * 
+ * 1. It adds all of the requisite <link> elements to preload Google Fonts
+ * 2. It collects any "critical path" CSS rendered by our Stitches instance
+ *    (since the last call to reset(), which prevents accumulation of CSS
+ *    from other pages when running in a server that renders multiple pages
+ *    in the same process.
+ * 3. It renders a <style> element with the critical path CSS and passes
+ *    along any additional props.
+ * 
+ * @param props
+ * @returns a fragment suitable for use in <head>, Helmet, next/head, etc.
+ */
+export function SSRStyle ({ googleFonts = DEFAULT_GOOGLE_FONTS, ...rest }: SSRStyleProps) {
   const css = getCssText()
   reset()
-  return <style id='sfgov-css' {...props} dangerouslySetInnerHTML={{ __html: css }} />
+  const googleFontsUrl = getGoogleFontsURL(googleFonts)
+  return <>
+    <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='anonymous' />
+    <link rel='preload' as='style' href={googleFontsUrl} />
+    <link rel='stylesheet' href={googleFontsUrl} />
+    <style id='sfgov-ssr-css' {...rest} dangerouslySetInnerHTML={{ __html: css }} />
+  </>
 }
