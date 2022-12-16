98ea562524bd1858eab7cec7134a47c561c4e994
Shawn Allen <shawnbot@users.noreply.github.com>
2022-12-16 10:12:28 -0800

    chore: delete landingDesc from constants

--- website/constants.js
+++ website/constants.js
@@ -4,7 +4,6 @@ const { repository } = defaultPackage
 const [owner, repo] = repository.split('/')
 const repoUrl = `https://github.com/${repository}`
 const treeBaseUrl = `${repoUrl}/tree/${defaultBranch}`
-const landingDesc = 'The SF.gov design system helps anyone creating digital products for San\xa0Francisco to ensure the interfaces are accessible, consistent, and\xa0efficient.'
 
 module.exports = {
   defaultPackage,
@@ -13,6 +12,5 @@ module.exports = {
   owner,
   repo,
   repoUrl,
-  treeBaseUrl,
-  landingDesc
+  treeBaseUrl
 }
